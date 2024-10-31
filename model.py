3c1d952933579822a0b6e2f75ca5144c4c6daaa6
AmadUdDinGakkhar <a_gakkhar@hotmail.com>
2024-10-31 12:18:50 +0500

    Created Decoder

--- model.py
+++ model.py
@@ -329,10 +329,52 @@ class Encoder(nn.Module):
             x = layer(x, mask)
         return self.norm(x)
 
-    # In the EncoderBlock class, a lambda function is used in the first layer 
-    # of residual_connection to wrap the self.self_attention_block function, 
+    # In the EncoderBlock class, a lambda function is used in the first layer
+    # of residual_connection to wrap the self.self_attention_block function,
     # but not in the second layer.
 
-    # The reason for this difference is that self.self_attention_block expects 
-    # four arguments (q, k, v, and mask), but self.feed_forward_block expects 
+    # The reason for this difference is that self.self_attention_block expects
+    # four arguments (q, k, v, and mask), but self.feed_forward_block expects
     # only one argument (x).
+
+
+class DecoderBlock(nn.Module):
+    def __init__(
+        self,
+        self_attention_block: MultiHeadAttentionBlock,
+        cross_attention_block: MultiHeadAttentionBlock,
+        feed_forward_block: FeedForwardBlock,
+        dropout: float,
+    ):
+        super().__init__()
+        self.self_attention_block = self_attention_block
+        self.cross_attention_block = cross_attention_block
+        self.feed_forward_block = feed_forward_block
+        self.residual_connection = nn.ModuleList(
+            [ResidualConnection(dropout) for _ in range(3)]
+        )
+
+    def forward(self, x, encoder_output, src_mask, trg_mask):
+        x = self.residual_connection[0](
+            x, lambda x: self.self_attention_block(x, x, x, trg_mask)
+        )
+        x = self.residual_connection[1](
+            x,
+            lambda x: self.cross_attention_block(
+                x, encoder_output, encoder_output, src_mask
+            ),
+        )
+        x = self.residual_connection[2](x, self.feed_forward_block)
+        return x
+
+
+class DecoderModule(nn.Module):
+    def __init__(self, layers: nn.ModuleList) -> None:
+        super().__init__()
+        self.layers = layers
+        self.norm = LayerNorm()
+
+    def forward(self, x, encoder_output, src_mask, trg_mask):
+        for layer in self.layers:
+            x = layer(x, encoder_output, src_mask, trg_mask)
+        return self.norm(x)
